01a8c243674d26a463804fb5982cab8b282588f4
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2021-06-14 17:14:17 +0100

    updated the user api

--- src/users/user/user.controller.ts
+++ src/users/user/user.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, ParseIntPipe, Patch } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Patch(':id')
+  updateUserById(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<UserDto> {
+    return this.userService.updateUserById(id, updateUserDto);
+  }
 }
--- src/users/user/user.service.ts
+++ src/users/user/user.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserDto } from './dto/user.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -9,4 +11,17 @@ export class UserService {
     @InjectRepository(User)
     private userRepository: Repository<User>,
   ) {}
+
+  async updateUserById(
+    id: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UserDto> {
+    try {
+      const user = await this.userRepository.findOneOrFail(id);
+      const newUser = { ...user, ...updateUserDto };
+      return await this.userRepository.save(newUser);
+    } catch (error) {
+      throw new UnauthorizedException("You're not authorized");
+    }
+  }
 }
