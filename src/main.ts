7cde0a8f2e27e7a6e76e82086b14da8903808c15
mahdibouaziz <mahdibouaziz@hotmail.fr>
2021-09-07 22:47:22 +0100

    make it to the production

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions = {
-    origin: ['http://localhost:4200'],
+    origin: ['*'],
   };
   app.enableCors(corsOptions);
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
